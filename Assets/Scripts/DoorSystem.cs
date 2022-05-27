1d679f7985257af9d6ad4d3954e96c64c0266202
nicolas29diaz <nicolas_san.diaz@uao.edu.co>
2022-05-26 23:09:40 -0500

    Puerta con llave funcionaaaaaaaa!!!(un par de bugs nada del otro mundo, creo... :v)

--- Assets/Scripts/DoorSystem.cs
+++ Assets/Scripts/DoorSystem.cs
@@ -4,30 +4,38 @@ using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 public class DoorSystem : MonoBehaviour
 {
+    
     public XRGrabInteractable doorGrab;
+    public HandleKey handleKey;
+    //public Rigidbody rb;
+    private bool key;
+
     // Start is called before the first frame update
     void Start()
     {
         doorGrab = GetComponent<XRGrabInteractable>();
+        //rb = GetComponent<Rigidbody>();
         doorGrab.enabled = false;
+        //rb.isKinematic = true;
+        //rb.detectCollisions = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.gameObject.tag == "Key")
+        key = handleKey.key;
+       
+        if (key)
         {
             doorGrab.enabled = true;
         }
         else
         {
             doorGrab.enabled = false;
+            Debug.Log("False");
         }
     }
+
+    
  
 }
